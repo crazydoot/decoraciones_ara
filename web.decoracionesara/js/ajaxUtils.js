202835f2e35eb8ca638722fe384770291ea916b1
AMZ <amartinezundmx@gmail.com>
2019-06-23 16:16:26 -0700

    Modified to get firestore data and load categories dynamically.

--- web.decoracionesara/js/ajaxUtils.js
+++ web.decoracionesara/js/ajaxUtils.js
@@ -3,6 +3,11 @@
 // Set up a namespace for our utility
 var ajaxUtils = {};
 
+// Get Firesstore DB reference and save to current namespace
+var dataBase = firebase.firestore();
+ajaxUtils.firestoreDB = dataBase;
+
+
 
 // Returns an HTTP request object
 function getRequestObject() {
@@ -22,39 +27,66 @@ function getRequestObject() {
 
 // Makes an Ajax GET request to 'requestUrl'
 ajaxUtils.sendGetRequest = 
-  function(requestUrl, responseHandler, isJsonResponse) {
-    var request = getRequestObject();
-    request.onreadystatechange = 
+  function(requestUrl, responseHandler, isFirestore) {
+    if(isFirestore == undefined) {isFirestore = false;}
+    if(isFirestore) {
+      handleResponse(requestUrl, responseHandler, isFirestore);
+    } else {
+      var request = getRequestObject();
+      request.onreadystatechange = 
       function() { 
-        handleResponse(request, responseHandler,isJsonResponse); 
+        handleResponse(request, responseHandler); 
       };
-    request.open("GET", requestUrl, true);
-    request.send(null); // for POST only
+      request.open("GET", requestUrl, true);
+      request.send(null); // for POST only
+    }
+    
+    
+    
   };
 
 
 // Only calls user provided 'responseHandler'
 // function if response is ready
 // and not an error
-function handleResponse(request,
-                        responseHandler,
-                        isJsonResponse) {
-  if ((request.readyState == 4) && (request.status == 200)) {
-
-    // Default to isJsonResponse = true
-    if (isJsonResponse == undefined) {
-      isJsonResponse = true;
-    }
-
-    if (isJsonResponse) {
-      responseHandler(JSON.parse(request.responseText));
-    }
-    else {
+function handleResponse(request, responseHandler, isFirestore) {
+  if(isFirestore){
+    getFirestoreCategories(request, responseHandler);
+    console.log("In handleResponse: request=",request);
+  } else if ((request.readyState == 4) && (request.status == 200)) {
       responseHandler(request.responseText);
-    }
   }
 }
 
+
+//
+//
+//
+function getFirestoreCategories(cat, handler){
+  var categoryDataList = [];
+  var category;
+  switch(cat){
+    case 1: category = "decoraciones"; break;
+    case 2: category = "arreglos"; break;
+    case 3: category = "postres"; break;
+    default: break;
+  }
+  console.log("CAT: ", category);
+  var i = 0;
+  var categoryRef = dataBase.collection("categorias").doc(category).collection("imagenes");
+  categoryRef.get().then(function(querySnapshot){
+    querySnapshot.forEach(function(doc){
+      //handler(doc.data());
+      categoryDataList.push(doc.data());
+      console.log("Printing Single Item#", i ,": ",doc.data());
+      i++;
+    });
+    console.log("DATA LIST: \n", categoryDataList);
+    handler(categoryDataList);
+  }).catch(function(err){
+    console.log("ERROR: ", err);
+  });
+}
 
 // Expose utility to the global object
 global.$ajaxUtils = ajaxUtils;
--- web.decoracionesara/js/scripts.js
+++ web.decoracionesara/js/scripts.js
@@ -3,11 +3,14 @@
 
 var aradeco = {};
 
-var homeHTML = "snippets/home-snippet.html"
+var homeHTML = "snippets/home-snippet.html";
+var allCategoriesURL = "";
+var categoriesTitleUrl = "snippets/category-title.html";
+var categoryHTML  = "snippets/category-snippet.html";
 
 var insertHTML = function (selector, html) {
 	var element = document.querySelector(selector);
-	element.insertHTML = html;
+	element.innerHTML = html;
 }
 
 
@@ -15,6 +18,12 @@ var showLoading = function (selector) {
 	var html = "<div class='text-center'>";
 	html += "<img src='media/loaging.gif'></div>";
 	insertHTML(selector, html);
+}
+
+var insertProperty = function (string, propName, propValue){
+	var propToReplace = "{{" + propName + "}}";
+	string = string.replace(new RegExp(propToReplace, "g"), propValue);
+	return string;
 }
 
 document,addEventListener("DOMContentLoaded", function(event){
@@ -25,6 +34,91 @@ document,addEventListener("DOMContentLoaded", function(event){
 	false);
 });
 
+aradeco.loadCategory = function () {
+	showLoading("#main-content");
+	$ajaxUtils.sendGetRequest(2, buildAndShowCategoriesHTML, true)
+}
+
+
+// Builds HTML for the categories page based on the data
+// from the server
+function buildAndShowCategoriesHTML (categories) {
+	console.log(categories,length)
+	if(categories == undefined) {
+		console.log("Categories is undefined!");
+	} else {
+	  // Load title snippet of categories page
+		$ajaxUtils.sendGetRequest(categoriesTitleUrl, function (categoriesTitleHtml) {
+	    	  // Retrieve single category snippet
+	    	$ajaxUtils.sendGetRequest(categoryHTML, function (categoryHtml) {
+	      		var categoriesViewHtml = buildCategoriesViewHtml(categories, categoriesTitleHtml, categoryHtml);
+	        	insertHTML("#main-content", categoriesViewHtml);
+	    	},
+	    	false);
+	    },
+	    false);
+	}
+}
+
+
+// Using categories data and snippets html
+// build categories view HTML to be inserted into page
+function buildCategoriesViewHtml(categories, categoriesTitleHtml, categoryHtml) {
+
+  var finalHtml = categoriesTitleHtml;
+  finalHtml += "<section id='items-section' class='col-sm-12 col-md-12 col-lg-10'>";
+
+  // Loop over categories
+  for (var i = 0; i < categories.length; i++) {
+  	console.log("Looping: ", i);
+    // Insert category values
+    var html = categoryHtml;
+    var name = "" + categories[i].title;
+    var category = categories[i].title;
+    html = insertProperty(html, "name", name);
+    html = insertProperty(html, "category", category);
+    finalHtml += html;
+  }
+
+  finalHtml += "</section> </div>";
+  return finalHtml;
+}
+
+
 global.$aradeco = aradeco;
 
 })(window);
+
+
+function testFirebase(){
+	/*var db = firebase.firestore();
+	
+	var batch = db.batch();
+
+	var docTitle = "arreglo-00";
+	var docData = {
+		title: "TITLE",
+		des: "SAME",
+		src:""
+	}
+	var i =0;
+	for(;i<10;i++){
+		var newTitle = docTitle + i;
+		var currRef = db.collection("categorias").doc("arreglos").collection("imagenes").doc(newTitle);
+		batch.set(currRef, docData)
+		console.log("WORKING" + newTitle + "\n")
+	}
+
+	batch.commit().then(function(){
+		console.log("ADDED THEM");
+	});*/
+
+
+
+	/*
+	var newRef = db.doc("categorias/arreglos/imagenes/arreglo-001")
+	newRef.get().then(function (doc) {
+		var data = doc.data();
+		console.log(JSON.stringify(data) + ":::::::::::::::");
+	});*/
+};
